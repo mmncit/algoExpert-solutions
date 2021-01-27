d42370df26ca0649aa08866f5775e821960ef3ad
mamun <mamun.rashid@seequent.com>
2021-01-27 13:24:50 -0700

    two number sum solution

---
+++ src/solutions/twoNumSum/README.md
@@ -0,0 +1,3 @@
+# Two Number sum
+
+An array of distinct numbers and a target sum are given. The task is to find out the pair of numbers which produces the target sum.
--- src/solutions/twoNumSum/twoNumSum.ts
+++ src/solutions/twoNumSum/twoNumSum.ts
@@ -1,6 +1,11 @@
 export function twoNumberSum(array: number[], targetSum: number): number[] {
   let dict: { [key: number]: number } = {};
-  for (let i = 0; i < array.length; i++) {}
-  console.log({ targetSum });
-  return [11, -1];
+  for (let i = 0; i < array.length; i++) {
+    if (dict.hasOwnProperty(array[i])) {
+      return [dict[array[i]], array[i]];
+    } else {
+      dict[targetSum - array[i]] = array[i];
+    }
+  }
+  return [];
 }
